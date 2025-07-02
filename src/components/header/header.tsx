b7599991564f43d499aa36cc048bb4c35745bbc2
Raphael Correia Dornelas <dornelasrapha@gmail.com>
2025-07-02 15:27:34 -0300

    hotfix: correcting imports

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,5 +1,5 @@
 import Link  from "next/link"
-import { Button } from "../button"
+import { Button } from "../ui/button"
 import { ActiveLink } from "../active-link";
 
 export const Header = () => {    
