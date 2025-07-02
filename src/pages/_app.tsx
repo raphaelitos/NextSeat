9da3e553fe6e14c8d62b526df5af830c592632ef
Raphael Correia Dornelas <dornelasrapha@gmail.com>
2025-07-02 15:36:58 -0300

    retirando html de _app (deu erro)

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,10 +4,8 @@ import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
   return(
-    <html className="dark">
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
-    </html>
+    <Layout>
+      <Component {...pageProps} />
+    </Layout>
   );
 }
