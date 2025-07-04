9d5aa21fadc109ee042dd140082c1e6ee02eded4
Raphael Correia Dornelas <dornelasrapha@gmail.com>
2025-07-04 09:10:05 -0300

    comecando hero e ajeitando tailwindconfig para setar corretamente o fontsize

---
+++ src/components/hero-section/hero-section.tsx
@@ -0,0 +1,11 @@
+export const HeroSection = () => {
+    return(
+        <section className="mt-16">
+            <div>
+                <div>
+                    <h1 className="text-gray-100 text-heading-hg">Venda seus produtos como afiliado em um único lugar</h1>
+                </div>
+            </div>
+        </section>
+    )
+}
