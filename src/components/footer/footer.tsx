1e7f1f09ba15e4cc633a2f73717f6c00ff5e39dc
Raphael Correia Dornelas <dornelasrapha@gmail.com>
2025-07-04 00:02:06 -0300

    aplicando configs do figma no tailwindconfigs

--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -3,15 +3,15 @@ import { Logo } from "../logo"
 
 export const Footer = () => {
     return(
-        <footer className="border-t border-white/10">
+        <footer className="bg-gray-500">
             <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
                 <div className="flex justify-between md:flex-row gap-8 py-8">
                    <Logo/>
 
-                    <nav className="flex flex-col md:flex-row items-center gap-4 text-sm text-muted-foreground">
-                        <Link href="/termos-uso" className="hover:text-primary">Termos de Uso</Link>
-                        <Link href="/politica-privacidade" className="hover:text-primary">Politica de Privacidade</Link>
-                        <Link href="/feedbakc" className="hover:text-primary">Feedback</Link>
+                    <nav className="flex flex-col md:flex-row items-center gap-4 text-sm text-blue-100">
+                        <Link href="/termos-uso" className="hover:text-blue-200">Termos de Uso</Link>
+                        <Link href="/politica-privacidade" className="hover:text-blue-200">Politica de Privacidade</Link>
+                        <Link href="/feedbakc" className="hover:text-blue-200">Feedback</Link>
                     </nav>
                 </div>
             </div>
