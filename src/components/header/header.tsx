94e5b61fb9ab8c9e1aa19d2dccb5c66832d690a1
Raphael Correia Dornelas <dornelasrapha@gmail.com>
2025-07-04 09:48:28 -0300

    mudando header para sticky

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -6,7 +6,7 @@ import { Logo } from "../logo";
 export const Header = () => {    
     
     return(
-        <header className="fixed top-0 z-50 w-full border-b 
+        <header className="sticky top-0 z-50 w-full border-b 
         border-white/10 bg-background/95 backdrop-blur
         supports-[backdrop-filter]:bg-background/60">
             <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
